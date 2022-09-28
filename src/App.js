1fef11899f021b82c9caf15ccc417cbad817c9c5
natan <natanbriman@gmail.com>
2022-09-28 23:59:45 +0300

    Feat(NavBar.js, App.js): Creating city select and state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import { Card } from 'react-bootstrap';
 import ForecastTable from './Components/ForecastTable';
 import NavBar from './Components/NavBar';
 
+const CITIES = [
+  'Haifa',
+  'Paris',
+  'London',
+  'New York',
+  'Los Angeles',
+  'Moscow',
+];
+const DAYS_TO_SHOW = 7;
+
 export const App = () => {
+  const [city, setCity] = useState(CITIES[0]);
+
   return (
     <>
-      <NavBar title="Natan's Weather App" />
+      <NavBar cities={CITIES} setCity={setCity} title="Natan's Weather App" />
       <Card className='mt-2 ms-2 me-2'>
-        <ForecastTable city={'London'} daysAmount={7} />
+        <ForecastTable city={city} daysAmount={DAYS_TO_SHOW} />
       </Card>
     </>
   );
