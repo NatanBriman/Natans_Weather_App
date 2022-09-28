3deec83375d2929c42bd2ad65c797275859b3390
natan <natanbriman@gmail.com>
2022-09-28 19:23:59 +0300

    Feat(WeatherCard.js): Creating Weather Card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import NavBar from './Components/NavBar';
+import WeatherCard from './Components/WeatherCard';
+import { Col, Container, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const CURRENT_WEATHER = {
+  day: {
+    avgtemp_c: 27.8,
+    condition: {
+      icon: '//cdn.weatherapi.com/weather/64x64/day/116.png',
+    },
+  },
+  date: '2022-09-28',
+};
 
 export const App = () => {
   return (
     <>
       <NavBar title="Natan's Weather App" />
+      <Container className='mt-2'>
+        <Row>
+          <Col>
+            <WeatherCard weather={CURRENT_WEATHER} />
+          </Col>
+          <Col>
+            <WeatherCard weather={CURRENT_WEATHER} />
+          </Col>
+          <Col>
+            <WeatherCard weather={CURRENT_WEATHER} />
+          </Col>
+          <Col>
+            <WeatherCard weather={CURRENT_WEATHER} />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
---
+++ src/Components/WeatherCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const formatDate = (date) =>
+  date.toLocaleString('default', { weekday: 'long' });
+
+export default function WeatherCard({ weather }) {
+  return (
+    <Card style={{ height: '80vh' }} className='text-center'>
+      <Col>
+        <h2>
+          <strong style={{ textDecoration: 'underline' }}>
+            {formatDate(new Date(weather.date))}
+          </strong>
+        </h2>
+      </Col>
+
+      <Col>
+        <h1>{weather.day.avgtemp_c}°</h1>
+      </Col>
+
+      <hr />
+
+      <Col border='primary'>
+        <img src={weather.day.condition.icon} />
+      </Col>
+    </Card>
+  );
+}
